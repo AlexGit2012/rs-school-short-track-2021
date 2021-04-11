a2eadd65dc3e211a6415d4aa6bab9458d11f7895
a.karavaychik@gmail.com <a.karavaychik@gmail.com>
2021-04-11 23:46:04 +0300

    feat: task1 complete

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,22 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  const arr = [...matrix];
+  const width = arr[0].length;
+  const height = arr.length;
+  let sum = 0;
+
+  for (let i = 0; i < width; i++) {
+    for (let y = 0; y < height; y++) {
+      if (arr[y][i] !== 0) sum += arr[y][i];
+      else break;
+    }
+  }
+
+  return sum;
+
+  // throw new Error('Not implemented');
 }
 
 module.exports = getMatrixElementsSum;
