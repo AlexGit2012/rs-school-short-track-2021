db35b6b932bdaa7208525441e2db5292dfb491a4
a.karavaychik@gmail.com <a.karavaychik@gmail.com>
2021-04-12 22:44:24 +0300

    feat: task15 complete

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,9 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  return email.split('@').reverse()[0];
+  // throw new Error('Not implemented');
 }
 
 module.exports = getEmailDomain;
