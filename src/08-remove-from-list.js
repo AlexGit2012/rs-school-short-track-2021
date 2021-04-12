72287ca5b42d7d84bb9e9a7ab40fc65ca766edbe
a.karavaychik@gmail.com <a.karavaychik@gmail.com>
2021-04-13 00:34:39 +0300

    feat: task8 complete"

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -1,3 +1,5 @@
+const ListNode = require('../extensions/list-node');
+
 /**
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
@@ -17,14 +19,34 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  // const arr = [...l];
-  // arr.forEach((el, index) => {
-  //   if (el===l) {
-  //
-  //   }
-  // })
-  throw new Error('Not implemented');
+function removeKFromList(l, k) {
+  if (Object.entries(l).length === 0) return l;
+
+  let result;
+  let resNode;
+  let currentNode = l;
+  while (!result) {
+    if (currentNode.value === k) currentNode = currentNode.next;
+    else {
+      result = new ListNode(currentNode.value);
+      resNode = result;
+      currentNode = currentNode.next;
+    }
+    if (!currentNode) break;
+  }
+
+  while (currentNode) {
+    if (currentNode.value === k) {
+      currentNode = currentNode.next;
+    } else {
+      resNode.next = currentNode;
+      resNode = resNode.next;
+      currentNode = currentNode.next;
+    }
+  }
+
+  return result;
+  // throw new Error('Not implemented');
 }
 
 module.exports = removeKFromList;
