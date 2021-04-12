d64efdd4818432210b2126c777f22f97e1d23483
a.karavaychik@gmail.com <a.karavaychik@gmail.com>
2021-04-12 21:40:48 +0300

    feat: task11 complete

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str.length === 0) return str;
+
+  let letter = str[0];
+  let count = 0;
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== letter) {
+      result += (count === 1 ? letter : count + letter);
+      count = 1;
+      letter = str[i];
+    } else count++;
+    if (i === str.length - 1) {
+      result += (count === 1 ? letter : count + letter);
+    }
+  }
+  return result;
+  // throw new Error('Not implemented');
 }
 
 module.exports = encodeLine;
